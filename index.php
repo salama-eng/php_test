7ad7f839e557955964cd2a4ee4d16b0d753933ff
salama <a541996salamh@gmail.com>
2022-03-08 23:25:59 +0300

    commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<?php
+session_start();
+
+print_r($_SESSION);
+
+
+
+$connect=mysqli_connect("localhost","root","","php_test");
+if(isset($_POST["add_to_cart"]))
+{
+  if(isset($_SESSION["shopping_cart"]))
+  {
+    $item_array_id=array_column( $_SESSION["shopping_cart"],'item_id');
+    if(!in_array($_GET['id'],$item_array_id))
+    {
+        $count=count($_SESSION["shopping_cart"]);
+        $item_array=array(
+        "item_id"  =>$_GET["id"],
+        "item_name"  =>$_GET["hidden_name"],
+        "item_price"  =>$_GET["hidden_price"],
+        "item_quantity"  =>$_GET["quantity"]
+        );
+        $_SESSION["shopping_cart"][$count]= $item_array;
+    }
+    else{ echo'<script>alert("item already added" )</script>';
+      echo'<script>window.location="index.php"</script>';
+      
+
+    }
+  }else
+  {
+    $item_array=array(
+      "item_id"  =>$_GET["id"],
+      "item_name"  =>$_GET["hidden_name"],
+      "item_price"  =>$_GET["hidden_price"],
+      "item_quantity"  =>$_GET["quantity"]
+    );
+  
+    $_SESSION["shopping_cart"][0]=$item_array;
+
+  }
+}
+
+if(isset($_GET["action"]))
+{
+  if($_GET["action"]=="delete")
+  {
+    foreach($_SESSION["shopping_cart"] as $keys=>$values)
+    {
+      if($values["item_id"]==$_GET["id"])
+      {
+        unset($_SESSION["shopping_cart"][$key]);
+        echo '<script> alert("item removed</script>';
+        echo'<script>window.location="index.php"</script>';
+      }
+    }
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="style.css" type="text/css" rel="stylesheet" media="all">
+  <title>Document</title>
+</head>
+<body> <h3>Shopping cart</h3>
+  <div class="container" style="display: flex;
+   justify-items: center;flex-direction: column;" >
+   <div style=" display:flex">
+    <?php
+$query="select * from products";
+
+      $result=mysqli_query($connect,$query);
+
+      if(mysqli_num_rows($result)!=0)
+      {
+        while($row=mysqli_fetch_assoc($result))
+        {
+         
+        ?>
+        <div clas="con" >
+          <form method="post" action="index.php?action=add&id=<?php echo $row["id"];?>">
+        
+        
+            <div class="con1">
+           
+              <img src="assets/<?php echo $row['product_img'];?>" alt="jee" width="180px">
+              <h4><?php echo $row['product_name'];?></h4>
+              <em>$<?php echo $row['price'];?></em>
+              <input type="number" name="quantity" value="1">
+              <input type="hidden" name="hidden_name" value="<?php echo $row['product_name'];?>">
+              <input type="hidden" name="hidden_price" value="<?php echo $row['price'];?>">
+              <input type="submit" name="add_to_cart" value="Add to cart">
+            </div>
+          </form>
+        </div>
+<?php
+
+        }
+      }
+    ?>
+ </div>
+ <div></div>
+ <h3>order datails cart</h3>
+ <div >
+
+   <table>
+     <tr>
+       <th>item name</th>
+       <th>Quantity</th>
+       <th>price</th>
+       <th>total</th>
+       <th>Action</th>
+     </tr>
+     <?php
+     if(!empty($_SESSION["shopping_cart"]))
+{
+  $total=0;
+  foreach($_SESSION['shopping_cart'] as $keys => $values)
+  {
+?>
+<tr>
+  <td><?php echo$values["item_name"]; ?></td>
+  <td><?php echo$values["item_quantity"]; ?></td>
+  <td><?php echo$values["item_price"]; ?></td>
+  <td><?php echo number_format($values["item_quantity"]*$values["item_price"],2) ?></td>
+  <td><a href="index.php?action=delete&id=<?php echo $values['item_id'];?>"> <span>Remove</span></a></td>
+</tr>
+
+
+
+<?php
+   $total=$total+($values["item_quantity"]*$values["item_price"]);
+   
+  }
+
+  ?>
+ 
+  <tr> 
+   <td>total</td>
+   <td>$<?php echo number_format($total,2);?></td>
+  </tr>
+  <?php
+}
+     ?>
+   </table>
+
+ </div>
+
+
+
+  </div>
+</body>
+</html>
---
+++ php_test.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2022 at 09:22 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) DEFAULT NULL,
+  `product_name` varchar(255) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `product_img` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_name`, `price`, `product_img`) VALUES
+(1, 'laptop', 500, 'lap.jpg'),
+(2, 'phone', 452, 'phone.jpg'),
+(3, 'wach hawawi', 899, 'watch.jpg'),
+(4, 'Mac workstation', 4528, 'mac.jpg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
